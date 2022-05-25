83fa1f7092ddcaaf5a622f1e22e78ae062bd8cfd
Aleksey Mitroshin <mitroshin.alex@gmail.com>
2022-05-25 12:19:32 +0300

    FIX PEP8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class AuthorPermissionOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS or
-                request.user == obj.author)
+        return (request.method in SAFE_METHODS
+                or request.user == obj.author)
 
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS or
-                request.user.is_authenticated)
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
